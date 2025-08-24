4f66c3716f871e52aeaf3e883a8f98e2958940a7
Robert Rasmussen <bobbyras90@gmail.com>
2025-08-24 15:23:20 -0700

    Fix player movement, add some boxes to the test scene

--- assets/player/player.gd
+++ assets/player/player.gd
@@ -1,38 +1,34 @@
 class_name Player
 extends CharacterBody3D
 
-const ACCELERATION: float = 2.0
-const STOP_FORCE: float = 30.0
+const ACCELERATION: float = 20.0
+const STOP_FORCE: float = 40.0
 const SPEED: float = 5.0
-const JUMP_VELOCITY: float = 4.5
+const JUMP_VELOCITY: float = 5.5;
 
 const GRAVITY: float = 9.8
+
+var _delta: float = 0
+var _in_air: bool = false
+
+func _process(delta: float):
+	_delta = delta
 
 func _physics_process(delta: float) -> void:
 	# Apply gravity
 	if !is_on_floor():
+		_in_air = true
 		velocity.y -= GRAVITY * delta
+	elif _in_air && is_on_floor():
+		_in_air = false
+		velocity.y = 0
 		
 	move_and_slide()
 
 func set_horizontal_velocity(direction: Vector2) -> void:
-	var to_add: Vector2 = direction * ACCELERATION
-	if to_add.x == 0:
-		var target_stop: float = STOP_FORCE * -sign(velocity.x)
-		if sign(target_stop) == sign(velocity.x):
-			target_stop = -velocity.x
-		to_add.x = target_stop
-	if to_add.y == 0:
-		var target_stop: float = STOP_FORCE * -sign(velocity.z)
-		if sign(target_stop) == sign(velocity.z):
-			target_stop = -velocity.z
-		to_add.y = target_stop
-	
-	var target_x: float = clamp(velocity.x + to_add.x, -SPEED, SPEED)
-	var target_z: float = clamp(velocity.z + to_add.y, -SPEED, SPEED)
-	
-	velocity.x = target_x
-	velocity.z = target_z
+	var target_velocity = direction * SPEED
+	velocity.x = target_velocity.x
+	velocity.z = target_velocity.y
 
 func try_jump() -> void:
 	if is_on_floor():
