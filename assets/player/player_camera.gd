bff615bd22eb95d7438cfe5d69be9e909635502f
Robert Rasmussen <bobbyras90@gmail.com>
2025-08-25 10:02:21 -0700

    Lower FOV change while sprinting

--- assets/player/player_camera.gd
+++ assets/player/player_camera.gd
@@ -1,11 +1,11 @@
 extends Camera3D
 
-@export var _sprint_fov = 75
-@onready var _starting_fov = self.fov
+@export var _sprint_fov: float = 80
+@onready var _starting_fov: float = self.fov
 
 func _on_player_is_sprinting_changed(is_sprinting: bool) -> void:
-	var tween = get_tree().create_tween()
+	var tween: Tween = get_tree().create_tween()
 	if is_sprinting:
 		tween.tween_property(self, "fov", _sprint_fov, 0.3).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
 	else:
-		tween.tween_property(self, "fov", _starting_fov, 0.3).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
+		tween.tween_property(self, "fov", _starting_fov, 0.5).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
