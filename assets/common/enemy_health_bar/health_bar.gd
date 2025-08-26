c8f5689c72ee9c0cc375305df48c2340286c3876
Robert Rasmussen <bobbyras90@gmail.com>
2025-08-25 22:31:31 -0700

    Add spawner

--- assets/common/enemy_health_bar/health_bar.gd
+++ assets/common/enemy_health_bar/health_bar.gd
@@ -1,9 +1,16 @@
+@tool
 class_name HealthBar
 extends Sprite3D
 
 @export var health: Health
+@export var size: Vector2:
+	set(value):
+		size = value
+		$SubViewport.size = size
 
 func _ready() -> void:
+	if Engine.is_editor_hint():
+		return
 	$SubViewport/ProgressBar.max_value = health.max_health
 	$SubViewport/ProgressBar.value = health.health
 	health.connect(health.health_changed.get_name(), _on_health_changed)
---
+++ assets/spawner/spawner.gd
@@ -0,0 +1,28 @@
+extends StaticBody3D
+
+@export var spawn_distance: float = 4.0
+
+const ENEMY_SCENES: Dictionary = {
+	0: preload("res://assets/enemy/enemy.tscn")
+}
+
+func _ready() -> void:
+	await get_tree().root.ready
+	spawn()
+
+func take_damage(amount: int) -> void:
+	$Health.health -= amount
+	
+func spawn() -> void:
+	var enemy: Enemy = ENEMY_SCENES[0].instantiate()
+	get_tree().root.add_child(enemy)
+	var facing: Vector3 = Vector3.FORWARD.rotated(Vector3.UP, randf_range(-PI, PI))
+	enemy.global_position = global_position + facing * spawn_distance
+	enemy.look_at(enemy.global_position + facing)
+
+func _on_timer_timeout() -> void:
+	spawn()
+
+
+func _on_health_died() -> void:
+	queue_free()
