08d02600af3a1d04e05764c39103b22f449607d9
Robert Rasmussen <bobbyras90@gmail.com>
2025-08-27 13:51:54 -0700

    Tweaks and bug fixes

---
+++ assets/enemy/enemy_pillar/enemy_pillar.gd
@@ -0,0 +1,35 @@
+class_name EnemyPillar
+extends AnimatableBody3D
+
+signal state_changed(new_state: EnemyState)
+
+@export var state_nodes: Dictionary[EnemyState, Node3D]
+
+var _target_states: Array = []
+
+enum EnemyState { Idle, Following, Jumping, Attacking }
+@onready var state: EnemyState = EnemyState.Idle:
+	set(value):
+		if state == value:
+			return
+		var state_node: Node3D = state_nodes[state]
+		if state_node and state_node.has_method("exit"):
+			state_node.exit()
+		state = value
+		state_node = state_nodes[state]
+		if state_node and state_node.has_method("enter"):
+			state_node.enter()
+		emit_signal(state_changed.get_name(), state)
+
+func _ready() -> void:
+	state_nodes[state].enter()
+
+func _process(_delta: float) -> void:
+	if len(_target_states) == 0:
+		return
+
+func take_damage(amount: int, knockback_amount: Vector3) -> void: 
+	$Health.health -= amount
+
+func _on_health_died() -> void:
+	queue_free()
--- assets/enemy/enemy_pillar/ground_detection.gd
+++ assets/enemy/enemy_pillar/ground_detection.gd
@@ -1,7 +1,9 @@
 extends Area3D
 
-@export var controller: Enemy
+@export var controller: EnemyPillar
 
 func _physics_process(delta: float) -> void:
+	if not monitoring:
+		return
 	if len(get_overlapping_bodies()) > 0:
 		controller.global_position.y += 0.05
