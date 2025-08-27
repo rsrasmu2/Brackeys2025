12065c167e50d61b88131a25890bd6a54f26e172
Robert Rasmussen <bobbyras90@gmail.com>
2025-08-26 20:41:16 -0700

    Add pillar enemy

---
+++ assets/enemy/enemy_pillar/pillar_idle.gd
@@ -0,0 +1,29 @@
+extends Node3D
+
+@export var controller: Enemy
+@export var target: EnemyTarget
+
+@export var distance_to_follow: float = 15.0
+var _sqr_follow_distance: float = distance_to_follow * distance_to_follow
+
+@export var distance_to_attack: float = 7.0
+var _sqr_attack_distance: float = distance_to_attack * distance_to_attack
+
+func _ready() -> void:
+	set_process(false)
+
+func _process(_delta: float) -> void:
+	if target.target == null:
+		return
+	
+	var dist = global_position.distance_squared_to(target.target.global_position)
+	if dist <= _sqr_attack_distance:
+		controller.state = controller.EnemyState.Attacking
+	elif dist <= _sqr_follow_distance:
+		controller.state = controller.EnemyState.Following
+
+func enter() -> void:
+	set_process(true)
+
+func exit() -> void:
+	set_process(false)
