26372741fa3b7f8b678acc3719986f1551a26fb3
Robert Rasmussen <bobbyras90@gmail.com>
2025-08-28 15:16:10 -0700

    Add Pickup Range powerup

--- assets/player/orb_pickup.gd
+++ assets/player/orb_pickup.gd
@@ -1,7 +1,11 @@
+class_name OrbPickup
 extends Area3D
 
-
 @export var experience: Experience
+@export var range: float = 4:
+	set(value):
+		range = value
+		$CollisionShape3D.shape.radius = range
 
 func _on_area_entered(area: Area3D) -> void:
 	experience.experience += area.value
