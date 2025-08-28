ecea4866e7331773549a512af9e7637862733aa3
Robert Rasmussen <bobbyras90@gmail.com>
2025-08-27 23:10:42 -0700

    Make errors happy

--- assets/powerups/bullet_damage_powerup.gd
+++ assets/powerups/bullet_damage_powerup.gd
@@ -6,4 +6,4 @@ func apply(player: Player) -> void:
 	player.get_node("PlayerCamera/Gun").connect("fired", _on_fired)
 
 func _on_fired(bullet: Bullet) -> void:
-	bullet.damage *= mult
+	bullet.damage = round(bullet.damage * mult)
