6efd0fce9310d1e2e0a25a69f4ec07fa6ab724e5
Robert Rasmussen <bobbyras90@gmail.com>
2025-08-28 18:36:26 -0700

    Improve things

--- assets/enemy/enemy_angel/angel_attack.gd
+++ assets/enemy/enemy_angel/angel_attack.gd
@@ -8,7 +8,6 @@ extends Node3D
 const LASER_SCENE := preload("res://assets/enemy/enemy_angel/angel_laser.tscn")
 
 func enter() -> void:
-	print("Entering Attack")
 	$"../AngelModel".look_at(target.target.global_position)
 	for spawn_point in fire_spawn_points:
 		spawn_point.look_at(target.target.global_position)
@@ -16,7 +15,6 @@ func enter() -> void:
 	$"../AngelModel/AnimationPlayer".play("attack")
 
 func exit() -> void:
-	print("Exiting Attack")
 	$"../AngelModel/AnimationPlayer".disconnect("animation_finished", _on_animation_finished)
 
 func fired() -> void:
@@ -24,7 +22,7 @@ func fired() -> void:
 		var laser = LASER_SCENE.instantiate()
 		get_tree().root.add_child(laser)
 		laser.global_transform = spawn_point.global_transform
-		laser.init(40, damage, controller)
+		laser.init(50, damage, controller)
 
 func _on_animation_finished(anim_name: String) -> void:
 	match anim_name:
