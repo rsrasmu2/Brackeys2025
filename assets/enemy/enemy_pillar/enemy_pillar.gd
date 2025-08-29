48a12ad19d42caacf9b4d2fe7813ae5abc895a08
Robert Rasmussen <bobbyras90@gmail.com>
2025-08-28 23:10:14 -0700

    Work on Pillar (and fix crash)

--- assets/enemy/enemy_pillar/enemy_pillar.gd
+++ assets/enemy/enemy_pillar/enemy_pillar.gd
@@ -4,7 +4,6 @@ extends AnimatableBody3D
 signal state_changed(new_state: EnemyState)
 
 @export var state_nodes: Dictionary[EnemyState, Node3D]
-
 @export var experience: int = 50
 
 var _target_states: Array = []
@@ -43,5 +42,5 @@ func _on_health_died() -> void:
 	var orb := EXP_SCENE.instantiate()
 	orb.init(experience)
 	get_tree().root.add_child(orb)
-	orb.global_transform = global_transform
+	orb.global_position = global_position
 	queue_free()
