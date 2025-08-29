785720191bc763b71c898949dfaa2943ce7853f4
Robert Rasmussen <bobbyras90@gmail.com>
2025-08-28 18:19:02 -0700

    Finish-ish adding Angel enemy

--- assets/enemy/enemy_angel/angel_follow.gd
+++ assets/enemy/enemy_angel/angel_follow.gd
@@ -5,7 +5,10 @@ extends Node3D
 
 var speed: float = 4
 
+@export var attack_range: float = 25
 @export var animation_player: AnimationPlayer
+
+@onready var _sqr_attack_range = attack_range * attack_range
 
 func _ready() -> void:
 	set_process(false)
@@ -20,15 +23,32 @@ func _physics_process(_delta: float) -> void:
 	controller.target_velocity.z = velocity.y
 
 func enter() -> void:
+	print("Entering Follow")
 	set_process(true)
-	animation_player.connect(animation_player.animation_finished.get_name(), _on_animation_finished)
-	animation_player.play("walk")
+	animation_player.connect("animation_finished", _on_animation_finished)
+	animation_player.play("run_start")
 
 func exit() -> void:
+	print("Exiting Follow")
 	set_process(false)
-	animation_player.disconnect(animation_player.animation_finished.get_name(), _on_animation_finished)
+	animation_player.disconnect("animation_finished", _on_animation_finished)
 	controller.target_velocity.x = 0
 	controller.target_velocity.z = 0
 
-func _on_animation_finished(_animation_name: String) -> void:
-	controller.state = controller.EnemyState.Idle
+func _on_animation_finished(anim_name: String) -> void:
+	match anim_name:
+		"run_start":
+			if global_position.distance_squared_to(target.target.global_position) <= _sqr_attack_range:
+				animation_player.play("run_end")
+			else:
+				animation_player.play("run_idle")
+		"run_idle":
+			if global_position.distance_squared_to(target.target.global_position) <= _sqr_attack_range:
+				animation_player.play("run_end")
+			else:
+				animation_player.play("run_idle")
+		"run_end":
+			if global_position.distance_squared_to(target.target.global_position) <= _sqr_attack_range:
+				controller.state = controller.EnemyState.Attacking
+			else:
+				animation_player.play("run_start")
