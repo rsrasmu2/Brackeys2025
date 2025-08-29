93506959fa847c2a2a59d0225fb9f946d7fca333
Robert Rasmussen <bobbyras90@gmail.com>
2025-08-28 20:23:05 -0700

    Angel works

--- assets/enemy/enemy_angel/angel_attack.gd
+++ assets/enemy/enemy_angel/angel_attack.gd
@@ -4,17 +4,43 @@ extends Node3D
 @export var target: EnemyTarget
 @export var fire_spawn_points: Array[Node3D]
 @export var damage: int = 15
+@export var rotation_speed: float = 180
 
 const LASER_SCENE := preload("res://assets/enemy/enemy_angel/angel_laser.tscn")
 
+var _should_fire: bool = false
+
+func _ready() -> void:
+	set_process(false)
+
+func _process(delta: float) -> void:
+	var target_pos: Vector3 = target.target.global_transform.origin
+	
+	var desired_x: Transform3D = $"../AngelModel".global_transform.looking_at(target_pos, Vector3.UP)
+	var from: Quaternion = $"../AngelModel".global_transform.basis.get_rotation_quaternion()
+	var to: Quaternion = desired_x.basis.get_rotation_quaternion()
+	
+	var angle := from.angle_to(to)
+	if _should_fire and angle <= PI / 32.0:
+		set_process(false)
+		$"../AngelModel".look_at(target.target.global_position)
+		for spawn_point in fire_spawn_points:
+			spawn_point.look_at(target.target.global_position)
+		$"../AngelModel/AnimationPlayer".play("attack")
+		return
+	
+	var max_step := deg_to_rad(rotation_speed) * delta
+	var t: float = min(1.0, max_step / angle)
+	var new: Quaternion = from.slerp(to, t)
+	$"../AngelModel".global_transform.basis = Basis(new)
+
 func enter() -> void:
-	$"../AngelModel".look_at(target.target.global_position)
-	for spawn_point in fire_spawn_points:
-		spawn_point.look_at(target.target.global_position)
+	_should_fire = true
 	$"../AngelModel/AnimationPlayer".connect("animation_finished", _on_animation_finished)
-	$"../AngelModel/AnimationPlayer".play("attack")
+	set_process(true)
 
 func exit() -> void:
+	set_process(false)
 	$"../AngelModel/AnimationPlayer".disconnect("animation_finished", _on_animation_finished)
 
 func fired() -> void:
@@ -27,6 +53,8 @@ func fired() -> void:
 func _on_animation_finished(anim_name: String) -> void:
 	match anim_name:
 		"attack":
+			_should_fire = false
+			set_process(true)
 			$"../AngelModel/AnimationPlayer".play("idle")
 		"idle":
 			controller.state = controller.EnemyState.Idle
